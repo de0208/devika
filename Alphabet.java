4a80e24a0f678520372e62bd04b358c789c8ae1a
de0208 <47915407+de0208@users.noreply.github.com>
2019-02-23 16:04:10 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+ class AlphabetFinder
+{
+    public static void main(String args[])
+    {
+        char character;
+        Scanner scanner = new Scanner(System.in);
+		
+        System.out.print("Enter a Character : ");
+        character = scanner.next().charAt(0);
+	    if((character>='a' && character<='z') || (character>='A' && character<='Z'))
+        {
+            System.out.print(character + " is an alphabet.");
+        }
+        else
+        {
+            System.out.print(character + " is not an alphabet.");
+        }
+    }
+}
+ 
